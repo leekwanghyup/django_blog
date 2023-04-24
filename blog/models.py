0ff4660ad653a2bad08e08f74064c185fc2c8695
leekwanghyup <leekwanghyup@naver.com>
2023-04-24 15:57:51 +0900

    부트ì이미지 파일 업로드

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,9 @@ class Post(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
+    # ImageField를 사용하려면 Pillow 라이브러리를 설치해야한다.
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d',blank=True)
+
     def __str__(self):
         return f'제목 :[{self.pk}] {self.title}'
 
--- myblog_prj/urls.py
+++ myblog_prj/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,3 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
