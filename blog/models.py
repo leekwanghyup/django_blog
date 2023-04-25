3a9160852cd10dfa6f45266923473c11851b36a9
leekwanghyup <leekwanghyup@naver.com>
2023-04-25 09:27:30 +0900

    file upload

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,8 @@ class Post(models.Model):
     # ImageField를 사용하려면 Pillow 라이브러리를 설치해야한다.
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d',blank=True)
 
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
+
     def __str__(self):
         return f'제목 :[{self.pk}] {self.title}'
 
